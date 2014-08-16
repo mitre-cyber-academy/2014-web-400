c1372d591820f1a304e0a1c2aebcabe784306ca9
Robert Clark <rbclark@mitre.org>
2014-08-16 05:23:44 -0400

    Updated recipe to actually work on amazon AWS.

---
+++ cookbooks/heartbleed-provisioner/README.md
@@ -0,0 +1,41 @@
+heartbleed-provisioner
+======================
+
+Name: Achy Breaky Heart
+
+Description: Users are provided with a heartbleed vulnerable server which they must exploit in order to obtain the private key of a user named jason which is used to decrypt a message found on a page of the site.
+
+Solution: First, the user will need to exploit the heartbleed vulnerability on the box, they can do so using [heartleech.](https://github.com/robertdavidgraham/heartleech) and the command `./heartleech host:port  --autopwn --threads 20`
+
+Next, they will need to copy the key to a file, (I called the file heartkey) and then run the command `chown 600 heartkey` in order to set the permissions to an acceptable value and run `ssh jason@host -i`pwd`/heartkey` to gain access to the box.
+
+Note: If you are using vagrant, you will need to append -p2222 to the ssh command, since vagrant uses a non-standard port.
+
+Once in, the user will need to find the right DOC file out of the 20,000 files (Tom Riddle 159962.doc). Running `cat Tom Riddle 159962.doc` will give the contents of the document. A link to the web page with the encrypted flag is in this document.
+
+The user must then go to that link, take the private key from the server by running `cat .ssh/id_rsa`, and decrypt the message. A Python scrypt, like the one below, can be written for decryption:
+	
+	def _decrypt_rsa(decrypt_key_file, cipher_text):
+    	'''
+    	Decrypt RSA encrypted package with private key
+    	:param decrypt_key_file: Private key
+    	:param cipher_text: Base64 encoded string to decrypt
+    	:return: String decrypted
+    	'''
+    	from Crypto.PublicKey import RSA
+    	from base64 import b64decode
+	
+    	key = open(decrypt_key_file, "r").read()
+    	rsakey = RSA.importKey(key)
+    	#optionally could use OAEP
+    	#from Crypto.Cipher import PKCS1_OAEP
+    	#rsakey = PKCS1_OAEP.new(rsakey)
+    	raw_cipher_data = b64decode(cipher_text)
+    	decrypted = rsakey.decrypt(raw_cipher_data)
+    	return decrypted
+	
+	decrypt_file = raw_input('Enter private key file\n')
+	text = raw_input('Enter encrypted data\n')
+	print _decrypt_rsa(decrypt_file,text)
+
+Flag: MCA-68656172
---
+++ cookbooks/heartbleed-provisioner/recipes/default.rb
@@ -0,0 +1,104 @@
+#
+# Cookbook Name:: heartbleed-provisioner
+# Recipe:: default
+#
+# Copyright 2014, MITRE
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'openssh::default'
+
+user_home = "/home/#{node["heartbleed-provisioner"]["user"]}"
+user_ssh_dir = "#{user_home}/.ssh"
+authorized_keys = "#{user_ssh_dir}/authorized_keys"
+
+remote_file "/tmp/openssl_1.0.1-4ubuntu5.11_amd64.deb" do
+  source "http://launchpadlibrarian.net/161943230/openssl_1.0.1-4ubuntu5.11_amd64.deb"
+  mode 0644
+end
+
+dpkg_package "openssl" do
+  source "/tmp/openssl_1.0.1-4ubuntu5.11_amd64.deb"
+  action :install
+end
+
+bash "lock-openssl" do
+  code "apt-mark hold openssl"
+  not_if "dpkg --get-selections | grep 'openssl' | grep 'hold'"
+end
+
+include_recipe "apache2::default"
+include_recipe "apache2::mod_ssl"
+
+remote_directory node["heartbleed-provisioner"]["challenge_www_root"] do
+  source "challenge-site"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+remote_directory user_home do
+  source "decoy-documents"
+  user node["heartbleed-provisioner"]["user"]
+  group node["heartbleed-provisioner"]["user"]
+  mode 0400
+end
+
+# Otherwise we cannot generate public key from ssh-keygen
+file node["heartbleed-provisioner"]["ssl_certificate_key"] do
+  mode 0600
+end
+
+web_app "challenge-site" do
+  server_name node['hostname']
+  server_port "443"
+  server_aliases ["*"]
+  docroot node["heartbleed-provisioner"]["challenge_www_root"]
+  enable true
+end
+
+package "lshell" do
+  action :install
+end
+
+cookbook_file "/etc/lshell.conf" do
+  source "lshell.conf"
+end
+
+user node["heartbleed-provisioner"]["user"] do
+  home user_home
+  shell "/usr/bin/lshell"
+  supports :manage_home => true
+  action :create
+end
+
+directory user_home do
+  mode 0500
+  user node["heartbleed-provisioner"]["user"]
+  group node["heartbleed-provisioner"]["user"]
+end
+
+directory user_ssh_dir do
+  action :create
+  mode 0500
+  user node["heartbleed-provisioner"]["user"]
+  group node["heartbleed-provisioner"]["user"]
+end
+
+bash "copy-private-key" do
+  code "ssh-keygen -y -f #{node['heartbleed-provisioner']['ssl_certificate_key']} > #{user_ssh_dir}/authorized_keys"
+end
+
+file authorized_keys do
+  mode 0400
+  user node["heartbleed-provisioner"]["user"]
+  group node["heartbleed-provisioner"]["user"]
+  action :touch
+end
+
+cookbook_file "#{user_ssh_dir}/id_rsa" do
+  source "id_rsa"
+  mode 0400
+  user node["heartbleed-provisioner"]["user"]
+  group node["heartbleed-provisioner"]["user"]
+end
